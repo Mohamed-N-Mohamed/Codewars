0ac28889b1b153e1a5d58574c702a4df3fc9926d
Mohamed-N-Mohamed <Mohamed-N-Mohamed@outlook.com>
2021-09-16 15:30:08 +0100

    7yu exercises

--- main.js
+++ main.js
@@ -34,3 +34,32 @@ function sumOfMinimums(arr) {
 }
 
 sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]])
+
+//You are given a string of letters and an array of numbers.
+//The numbers indicate positions of letters that must be removed, in order, starting from the beginning of the array.
+//After each removal the size of the string decreases (there is no empty space).
+//Return the only letter left.
+
+function lastSurvivor(letters, coords) {
+  letters = letters.split('');
+  console.log(letters)
+  for (let i = 0; i < coords.length; i++){
+   letters.splice(coords[i], 1)
+  }
+  return letters.join('')
+}
+
+
+console.log(lastSurvivor('abc', [1, 1]))
+
+
+//Write function which will create a string from a list of strings, separated by space.
+
+function wordsToSentence(words) {
+  return words.reduce((a, b) => a + ' ' + b)
+}
+
+
+console.log(wordsToSentence(["hello", "world"]))
+
+
